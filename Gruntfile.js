7456f37ca6d46df2ae0fce4e6bf6da41fe521396
ahmet kizilay <ahmet.kizilay@gmail.com>
2014-04-30 17:22:52 +0300

    added Grunt uglify

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*!<%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'history.js',
+                dest: 'history.min.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['uglify']);
+};
